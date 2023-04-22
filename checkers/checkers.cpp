88226899e9f97759dcff4a4c938659f9fd4b6faf
mirzakhanyaned <edmondik2@gmail.com>
2023-04-22 22:06:36 +0300

    Moving and eating checkers

--- checkers/checkers.cpp
+++ checkers/checkers.cpp
@@ -1,7 +1,18 @@
-#include <iostream>
-using namespace std;
-
-int main()
+bool isValidMove(char Board[8][8], int fromX, int fromY, int toX, int toY)
 {
-	cout << "Hello Yuriy" << endl;
-}
+    // Проверяем, что мы перемещаем шашку по диагонали
+    int dx = toX - fromX;
+    int dy = toY - fromY;
+    if (abs(dx) != abs(dy))
+        return false;
+    // Проверяем, что мы перемещаем шашку на свободную клетку
+    if (Board[toX][toY] != ' ')
+        return false;
+
+    // Проверяем, что мы перемещаем шашку на одну клетку или съедаем шашку противника
+    if (!(abs(dx) == 1 || abs(dx) == 2))
+        return false;
+
+    // Проверяем, что на пути хода есть шашка противника, которую можно съесть
+    if (abs(dx) == 2 && abs(dy) == 2 && Board[(fromX + toX) / 2][(fromY + toY) / 2] == ' ')
+        return false;
