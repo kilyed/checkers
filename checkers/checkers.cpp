453425e4da7ec2a4b5613cd7289586f314755a20
mirzakhanyaned <edmondik2@gmail.com>
2023-04-22 22:14:57 +0300

    GameOver

--- checkers/checkers.cpp
+++ checkers/checkers.cpp
@@ -82,3 +82,145 @@ bool isValidMove(char Board[8][8], int fromX, int fromY, int toX, int toY)
 
     return true;
 }
+bool movePiece(char Board[8][8], int fromX, int fromY, int toX, int toY)
+{
+    // ѕровер€ем, что мы перемещаем шашку на свободную клетку
+    if (Board[toX][toY] != ' ')
+        return false;
+
+    // ѕровер€ем, что мы перемещаем шашку по диагонали
+    if ((fromX + fromY) % 2 != (toX + toY) % 2)
+        return false;
+
+    // ѕровер€ем, что мы перемещаем шашку на одну клетку или съедаем шашку противника
+    if (!(abs(fromX - toX) == 1 || abs(fromX - toX) == 2))
+        return false;
+
+    // ѕровер€ем, что на пути хода есть шашка противника, если мы съедаем шашку
+    if (abs(fromX - toX) == 2 && abs(fromY - toY) == 2 && Board[(fromX + toX) / 2][(fromY + toY) / 2] == ' ')
+        return false;
+
+    // ≈сли ход допустим, перемещаем шашку на новую клетку
+    Board[toX][toY] = Board[fromX][fromY];
+    Board[fromX][fromY] = ' ';
+
+    // ≈сли на пути хода была шашка противника, то удал€ем ее
+    if (abs(toX - fromX) == 2 && abs(toY - fromY) == 2)
+        Board[(fromX + toX) / 2][(fromY + toY) / 2] = ' ';
+
+    // ≈сли шашка достигает последнего р€да противника, она превращаетс€ в дамку
+    if (toX == 0 && Board[toX][toY] == 'B') {
+        Board[toX][toY] = 'Q';
+        Board[fromX][fromY] = ' '; // удал€ем шашку с доски
+    }
+    else if (toX == 7 && Board[toX][toY] == 'A') {
+        Board[toX][toY] = 'K';
+        Board[fromX][fromY] = ' '; // удал€ем шашку с доски
+    }
+    // ѕолучаем тип шашки
+    char piece = Board[fromX][fromY];
+
+    // ≈сли шашка - дамка
+    if (piece == 'K' || piece == 'Q') {
+        // ѕровер€ем, что мы перемещаем дамку по диагонали
+        if (abs(fromX - toX) != abs(fromY - toY))
+            return false;
+    }
+    else {
+        // ѕровер€ем, что мы перемещаем обычную шашку вперед или назад
+        if (piece != 'A' && toX > fromX)
+            return false;
+        if (piece != 'B' && toX < fromX)
+            return false;
+        // ѕровер€ем, что мы перемещаем обычную шашку на одну клетку по диагонали
+        if (abs(fromX - toX) != 1 || abs(fromY - toY) != 1)
+            return false;
+    }
+
+
+    // ≈сли шашка - дамка, провер€ем возможность передвижени€ на любую свободную клетку по диагонали
+    if (piece == 'K' || piece == 'Q') {
+        int xDir = (toX - fromX) > 0 ? 1 : -1;
+        int yDir = (toY - fromY) > 0 ? 1 : -1;
+
+        int i = fromX + xDir;
+        int j = fromY + yDir;
+
+        while (i != toX && j != toY) {
+            if (Board[i][j] != ' ') {
+                return false;
+            }
+            i += xDir;
+            j += yDir;
+        }
+    }
+
+    return true;
+}
+
+
+
+
+
+bool isGameOver(char Board[8][8])
+{
+    bool whitePiecesLeft = false;
+    bool blackPiecesLeft = false;
+    // ѕровер€ем, есть ли еще белые или черные шашки на доске
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (Board[i][j] == 'A' || Board[i][j] == 'K')
+            {
+                whitePiecesLeft = true;
+            }
+            else if (Board[i][j] == 'B' || Board[i][j] == 'Q')
+            {
+                blackPiecesLeft = true;
+            }
+        }
+    }
+
+    // ≈сли либо у белых, либо у черных не осталось шашек, игра окончена
+    if (!whitePiecesLeft || !blackPiecesLeft)
+    {
+        return true;
+    }
+
+    // ≈сли белые или черные не могут сделать ход, игра окончена
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (Board[i][j] == 'A' || Board[i][j] == 'K')
+            {
+                for (int k = -1; k <= 1; k++)
+                {
+                    for (int l = -1; l <= 1; l++)
+                    {
+                        if (isValidMove(Board, i, j, i + k, j + l))
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            else if (Board[i][j] == 'B' || Board[i][j] == 'Q')
+            {
+                for (int k = -1; k <= 1; k++)
+                {
+                    for (int l = -1; l <= 1; l++)
+                    {
+                        if (isValidMove(Board, i, j, i + k, j + l))
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return true;
+}
