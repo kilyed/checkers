71d919d4466cb18e6d752062006480d94696d15a
kilyed <pon4ikig@gmail.com>
2023-04-22 22:03:38 +0300

    add board.

--- checkers/checkers.cpp
+++ checkers/checkers.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main()
+void Display(char Board[8][8])
 {
-	cout << "Hello Yuriy" << endl;
+    int i;
+    for (i = 0; i < 8; i++)
+    {
+        cout << " +---+---+---+---+---+---+---+---+" << endl;
+        cout << i + 1 << " | " << Board[i][0] << " | " << Board[i][1] << " | " << Board[i][2] << " | " << Board[i][3] << " | " << Board[i][4] << " | " << Board[i][5] << " | " << Board[i][6] << " | " << Board[i][7] << " |" << endl;
+    }
+    cout << "  +---+---+---+---+---+---+---+---+" << endl;
+    cout << "    A   B   C   D   E   F   G   H  " << endl;
 }
